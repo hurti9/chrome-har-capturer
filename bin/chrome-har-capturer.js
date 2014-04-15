3530d9673edc4eb83931a1b39504ff590bec78c7
Mats Hurtig <hurtigmats@gmail.com>
2014-04-15 15:22:21 +0200

    misspelled useragent, yeah

--- bin/chrome-har-capturer.js
+++ bin/chrome-har-capturer.js
@@ -35,7 +35,7 @@ var argv = require('optimist')
 	'request': {
 	    'method': 'Network.setUserAgentOverride',
 	    'params': {
-		'user-agent': 'Mozilla/5.0 (Linux; Android 4.3; Nexus 7 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.72 Safari/537.36'
+		'userAgent': 'Mozilla/5.0 (Linux; Android 4.3; Nexus 7 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.72 Safari/537.36'
 	     }
 	}
 	
