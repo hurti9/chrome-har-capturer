38dd6f14b5f1391af7c97a60384ec440bef37ef2
Mats Hurtig <hurtigmats@gmail.com>
2014-04-15 15:12:49 +0200

    modified user agent again

--- bin/chrome-har-capturer.js
+++ bin/chrome-har-capturer.js
@@ -33,7 +33,10 @@ var argv = require('optimist')
             'boolean': true
         },
 	'request': {
-		'user-agent'= 'Mozilla/5.0 (Linux; Android 4.3; Nexus 7 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.72 Safari/537.36'
+	    'method': 'Network.setUserAgentOverride',
+	    'params': {
+		'user-agent': 'Mozilla/5.0 (Linux; Android 4.3; Nexus 7 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.72 Safari/537.36'
+	     }
 	}
 	
     })
